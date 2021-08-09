a90f6e0f0c3fddaa5fb81c77b4e14c5891ba3ba0
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2021-08-09 18:29:57 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -52,7 +52,7 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Alfin Antony</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://instagram.com/alfinant0ny?utm_medium=copy_link">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://instagram.com/alfinant0ny?utm_medium=copy_link">Alfin Antony</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
